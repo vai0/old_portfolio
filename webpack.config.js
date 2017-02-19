268b7983577544aec90f36c43ceed827067f75f4
Justin Chi <j.chi2241@gmail.com>
2017-02-19 01:01:42 -0800

    optimize build for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
+const webpack = require('webpack');
+
 module.exports = {
+  devtool: 'cheap-module-source-map',
   entry: './js/main.js',
   output: {
     filename: 'bundle.js'
@@ -31,5 +34,13 @@ module.exports = {
       {test: /\.scss$/, loader: 'style!css!sass'},
       {test: /\.css$/, loader: 'style!css'},
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ],
 }
