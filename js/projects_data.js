7223ad6a3b4727c8edfe55c0ec62e7f034cace62
Justin Chi <j.chi2241@gmail.com>
2017-05-22 00:25:35 -0700

    tweak purple color, fix typos, fix project descriptions

--- js/projects_data.js
+++ js/projects_data.js
@@ -1,8 +1,8 @@
 const projects = [
   {
     title: "Chart the Stock Market",
-    description: 'This project is based off the challenge from <a href="https://www.freecodecamp.com/challenges/chart-the-stock-market">freeCodeCamp</a>. Made it prettier and added autocomplete functionality.',
-    skills: ['HTML5', 'CSS3', 'Javascript', 'ES6', 'ReactJS'],
+    description: 'This project is based off the challenge from <a href="https://www.freecodecamp.com/challenges/chart-the-stock-market">freeCodeCamp</a>. Data pulled from the Tradier API - search autocomplete relies on the lookup endpoint and runs slow at times, be patient!',
+    skills: ['HTML5', 'CSS3', 'JavaScript', 'ES6', 'ReactJS'],
     links: {
       github: 'https://github.com/vai0/stockmarketchart',
       preview: 'https://justinchi.me/stockmarketchart/'
@@ -10,8 +10,8 @@ const projects = [
   },
   {
     title: "Wakey Alarm Clock",
-    description: 'This project is based off a mock-up by designer <a href="http://www.xiaotongyuchi.com/wakeyalarm">Xiaotong Yuchi</a>. This project was intended to deepen my understanding of ReactJS. Polymer components were used for material design (strange I know). Eventual plan - create a progressive web app. Designed for mobile use.',
-    skills: ['HTML5', 'CSS3', 'Javascript', 'ReactJS', 'Polymer', 'Material Design'],
+    description: 'This project is based off a mock-up by designer <a href="http://www.xiaotongyuchi.com/wakeyalarm">Xiaotong Yuchi</a>. Polymer components were taken directly from Google spec to ensure smooth animations on mobile. Designed for mobile use.',
+    skills: ['HTML5', 'CSS3', 'JavaScript', 'ReactJS', 'Polymer', 'Material Design'],
     links: {
       github: 'https://github.com/vai0/alarmclock',
       preview: 'https://justinchi.me/alarmclock/'
@@ -19,8 +19,8 @@ const projects = [
   },
   {
     title: "Wakey Weather Backgrounds",
-    description: 'Backgrounds for when the alarm is triggered in the Wakey Alarm Clock app. CSS animations and a bit of Javascript for the snow and rain. No external libraries.',
-    skills: ['HTML5', 'CSS3', 'Javascript'],
+    description: 'Backgrounds for when the alarm is triggered in the Wakey Alarm Clock app. CSS animations and a bit of JavaScript for the snow and rain. No external libraries.',
+    skills: ['HTML5', 'CSS3', 'JavaScript'],
     links: {
       github: 'https://github.com/vai0/weather-animations',
       preview: 'https://justinchi.me/alarmclockweather'
@@ -28,7 +28,7 @@ const projects = [
   },
   {
     title: "Tom's Metal Site",
-    description: "A revamp the website of the company where I took my first job. This project was intended to deepen my understanding of media queries. You can see the original site here. The site supports mobile, tablet, and desktop sizes. All done with media queries, pure HTML5 and CSS3 - no external libraries or javascript.",
+    description: "A revamp the website of the company where I took my first job. This project was intended to deepen my understanding of media queries. You can see the original site here. The site supports mobile, tablet, and desktop sizes. All done with media queries, pure HTML5 and CSS3 - no external libraries or JavaScript.",
     skills: ['HTML5', 'CSS3'],
     links: {
       github: 'https://github.com/vai0/tomsmetal_site',
@@ -37,8 +37,8 @@ const projects = [
   },
   {
     title: 'Portfolio Site',
-    description: 'This website!',
-    skills: ['HTML5', 'CSS3', 'Javascript', 'ReactJS'],
+    description: 'This website! The site supports mobile and desktop.',
+    skills: ['HTML5', 'CSS3', 'JavaScript', 'ReactJS'],
     links: {
       github: 'https://github.com/vai0/portfolio_3',
       preview: 'https://justinchi.me/'
